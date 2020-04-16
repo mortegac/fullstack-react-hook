170423d7ee1ec5d29999a301a1afbf380cad048c
Manuel Ortega Carcamo <mortega@apgca.cl>
2020-04-15 23:56:33 -0400

    chore: ejemplo hooks useState

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const Header = (props) => {
+  
+  const [count, setCount] = useState(0);
+
+  return(
+    <>
+      <h1>{props.title}</h1>
+      <h6>Contador: {count}</h6>
+      <button 
+        onClick={() => setCount(count + 1)}>
+        Sumar contador
+      </button>
+    </>
+)}
+
+const App = () => {
   return (
     <div className="App">
+      <Header title="Hook useState" />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
